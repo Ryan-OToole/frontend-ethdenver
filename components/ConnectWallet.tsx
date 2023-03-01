3bb80889a6afc7cbec72527009f3692fe3d7a666
Ryan-Otoole <otoolerj@gmail.com>
2023-03-01 12:43:21 -0600

    trying to debug ethers environment and import issues

---
+++ components/ConnectWallet.tsx
@@ -0,0 +1,14 @@
+const ConnectWallet = () => {
+
+    const connectWallet = () => {
+        console.log('hi');
+    }
+
+
+    return (
+      <div>
+        <button type="button" className="btn btn-outline-secondary btn-lg px-4" onClick={connectWallet}>Connect Wallet</button>
+      </div>
+    )
+}
+export default ConnectWallet;
---
+++ components/CreateWallet.tsx
@@ -0,0 +1,8 @@
+const CreateWallet = () => {
+    return (
+      <div>
+        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Create Wallet</button>
+      </div>
+    )
+}
+export default CreateWallet;
